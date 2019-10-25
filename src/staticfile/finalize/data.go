66e5897e7dd2aa27a15cd7681be8f6f8c68bfebc
Flores Eken <flores@eken.nl>
2019-10-25 14:39:27 +0200

    Update data.go

--- src/staticfile/finalize/data.go
+++ src/staticfile/finalize/data.go
@@ -129,6 +129,11 @@ http {
         add_header Strict-Transport-Security "max-age=31536000{{if .HSTSIncludeSubDomains}}; includeSubDomains{{end}}{{if .HSTSPreload}}; preload{{end}}";
       {{end}}
 
+      location /assets/inject {
+        default_type application/json;
+        return 200 '<%= ENV["INJECT"] %>';
+    	}
+
       {{if ne .LocationInclude ""}}
         include {{.LocationInclude}};
       {{end}}
