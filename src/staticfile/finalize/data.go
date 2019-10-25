e180e01760045d220e3b9257fdf3099385229753
Flores Eken <flores@eken.nl>
2019-10-25 15:03:04 +0200

    Update data.go

--- src/staticfile/finalize/data.go
+++ src/staticfile/finalize/data.go
@@ -129,7 +129,7 @@ http {
         add_header Strict-Transport-Security "max-age=31536000{{if .HSTSIncludeSubDomains}}; includeSubDomains{{end}}{{if .HSTSPreload}}; preload{{end}}";
       {{end}}
 
-      location /assets/inject {
+      location /assets/inject.js {
         default_type application/json;
         return 200 '<%= ENV["INJECT"] %>';
     	}
